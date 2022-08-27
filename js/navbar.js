c21df9712ab17f909fb3e3b63741479c73c16195
celia <celiatosic@gmail.com>
2022-08-27 20:35:53 +0200

    navbar improvement

--- js/navbar.js
+++ js/navbar.js
@@ -6,17 +6,26 @@ const navbar = {
         navbar.burger = document.querySelector("#header-burger");
         navbar.nav = document.querySelector("#nav");
 
+        //On cible la croix
+        navbar.cross =document.querySelector("#header-cross");
+
         //On écoute le clic sur le burger menu
         navbar.burger.addEventListener("click", navbar.show);
+        navbar.cross.addEventListener("click", navbar.removeSideBar);
+
+
     },
 
     show: function(){
+        navbar.burger.classList.add("active");
         navbar.nav.classList.add("active");
-        navbar.burger.remove();
+        navbar.cross.classList.add("active");
     },
 
     //Enlève la classe active à la navbar pour la faire disparaître lorqu'on clique sur les liens du menu
     removeSideBar: function(){
-        navbar.navBar.classList.remove("active");
+        navbar.nav.classList.remove("active");
+        navbar.cross.classList.remove("active");
+        navbar.burger.classList.remove("active");
     },
 }
