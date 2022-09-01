2d593433ad98befd7b6b5adb41d81b0a317bd5e9
celia <celiatosic@gmail.com>
2022-09-01 18:57:16 +0200

    slider and contact ok have to clean the code

--- js/slider.js
+++ js/slider.js
@@ -3,59 +3,89 @@ const slider = {
     slideList: null,
     init: function () {
         console.log('slider module loading ...');
-        
-        // 3. on récupère toutes les slides dans notre module
-        slider.slideList = document.querySelectorAll('.slider__img');
 
         // 1. On récupére les 2 boutons pour leur ajouter un écouteur d'événement click
         const buttonsPrevious = document.querySelectorAll('.btn-previous');
         const buttonsNext = document.querySelectorAll('.btn-next');
-        console.log(buttonsPrevious);
-        console.log(buttonsNext);
        
         //Au moment du clic sur ces boutons (gauche ou droite), des fonctions sont exécutées
 
         for(const buttonPrevious of buttonsPrevious){
-            buttonPrevious.addEventListener('click', slider.handleLeftButtonClick);   
+            buttonPrevious.addEventListener('click', slider.handlePreviousButtonClick);      
         };
 
         for(const buttonNext of buttonsNext){
-            buttonNext.addEventListener('click', slider.handleRightButtonClick);
-
+            buttonNext.addEventListener('click', slider.handleNextButtonClick);      
         };
 
     },
 
-    //2. On définit la fonction handleLeftButtonCLick et handleRightButtonClick. Ces fonctions appellent plusieurs fonctions 
-    handleLeftButtonClick: function ()
-    {      
+    ImagesRelatedToTheSlider(event){
 
-        slider.hideSlide(slider.currentSlideIndex);
-        slider.moveToPreviousSlide();
-        slider.showSlide(slider.currentSlideIndex);
+        //On récupère le bouton sur lequel on a cliqué 
+        const child = event.target;
+        //On remonte jusqu'à son parent
+        const parent = child.parentNode;
+        //On récupère les classes du parent
+        const parentClasses = parent.classList;
+        //On récupère la classe qui différencie chaque slider
+        const classe = parentClasses[1];
+        //On met le selecteur dans une variable
+        const selector = '.' + classe + '> .slider__img';
+        //On récupère les images associées au slider en question
+        const slideListImages = document.querySelectorAll(selector);
+
+        return slideListImages;
+    },
+
+    handlePreviousButtonClick:function(event){
+
+        //On récupère le résultat de la fonction ImagesRelatedToTheSlider() (donc les images du slider) dans une variable slideList
+        const slideList = slider.ImagesRelatedToTheSlider(event);
+         
+        //On passe en paramètre des différentes fonction les images récupérées.
+        slider.hideSlide(slider.currentSlideIndex, slideList);
+        slider.moveToPreviousSlide(slideList);
+        slider.showSlide(slider.currentSlideIndex, slideList);
 
     },
 
-    handleRightButtonClick: function ()
-    {
+    handleNextButtonClick:function(event){
+         
+        const slideList = slider.ImagesRelatedToTheSlider(event);
+        
+        slider.hideSlide(slider.currentSlideIndex, slideList);
+        slider.moveToNextSlide(slideList);
+        slider.showSlide(slider.currentSlideIndex, slideList);
 
-        slider.hideSlide(slider.currentSlideIndex);
-        slider.moveToNextSlide();
-        slider.showSlide(slider.currentSlideIndex);
-       
-    },
+   },
+
 
     //4. On définit les fonctions 
-    hideSlide: function(slideIndexToHide)
+    hideSlide: function(slideIndexToHide, slideList)
     {
-
         // on veut récupérer la slide qui est à l'index slideIndexToHide
-        slider.slideList[slideIndexToHide].classList.remove('slider__img--current');
+       slideList[slideIndexToHide].classList.remove('slider__img--current');
     },
 
-    moveToNextSlide: function()
+    moveToPreviousSlide: function(slideList)
     {
-        if(slider.currentSlideIndex === slider.slideList.length - 1)
+
+        if(slider.currentSlideIndex === 0)
+        {
+            // si on est sur la première slide on change la valeur de currentSlide à la dernière slide
+            slider.currentSlideIndex = slideList.length - 1;
+        }
+        else {
+            // sinon on decrémente sa valeur
+            slider.currentSlideIndex--;
+        }
+    },
+
+    moveToNextSlide: function(slideList)
+    {
+       
+        if(slider.currentSlideIndex === slideList.length - 1)
         {
             // si on est sur la dernière slide on change la valeur de currentSlide à 0
             slider.currentSlideIndex = 0;
@@ -66,33 +96,9 @@ const slider = {
         }
     },
 
-    moveToPreviousSlide: function()
+    showSlide: function(slideIndexToShow, slideList)
     {
-        if(slider.currentSlideIndex === 0)
-        {
-            // si on est sur la première slide on change la valeur de currentSlide à la dernière slide
-            slider.currentSlideIndex = slider.slideList.length - 1;
-        }
-        else {
-            // sinon on decrémente sa valeur
-            slider.currentSlideIndex--;
-        }
-    },
-
-    showSlide: function(slideIndexToShow)
-    {
-        slider.slideList[slideIndexToShow].classList.add('slider__img--current');
+        slideList[slideIndexToShow].classList.add('slider__img--current');
     },
 
 }
-
-
-
-// const child = event.target;
-// const parent = child.parentNode;
-// // console.log(parent.classList);
-// const classes = parent.classList;
-// const classe = classes[1];
-
-// const list = '.' + classe + '> .slider__img';
-// const slideListImages = document.querySelectorAll(list);
